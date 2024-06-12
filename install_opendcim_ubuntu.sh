4980046314a88b7b205447c19ce26a9487b075b6
Carl Angelo Nievarez <carlangelopamparonievarez@gmail.com>
2024-06-12 23:22:18 +0800

    Update install_opendcim_ubuntu.sh

--- install_opendcim_ubuntu.sh
+++ install_opendcim_ubuntu.sh
@@ -1,72 +1,63 @@
-Install some additional packages needed
-Log in as the user set up during the installation, and at the $ prompt you'll do the following:
+#!/bin/bash
 
-$ sudo apt-get install php-snmp snmp-mibs-downloader php-curl php-gettext graphviz
-You will be asked to confirm, and once the packages are installed it is time to download the latest version of openDCIM.
+# Load variables from .env file
+ENV_FILE=".env"
+if [ -f "$ENV_FILE" ]; then
+    source "$ENV_FILE"
+else
+    echo "Error: $ENV_FILE not found."
+    exit 1
+fi
 
-Download and unpackage openDCIM
-Assuming that you wish to install openDCIM under the /var/www directory, do the following, but be sure to use the most recent package link for openDCIM:
+# Install required packages
+sudo apt-get update
+sudo apt-get install -y apache2 php php-snmp snmp-mibs-downloader php-curl php-gettext graphviz mariadb-server
 
-$ cd
-$ wget http://opendcim.org/packages/openDCIM-4.0.1.tar.gz
-$ cd /var/www
-$ sudo tar zxpvf ~/openDCIM-4.0.1.tar.gz
-$ sudo ln -s openDCIM-4.0.1 dcim
-Give Apache permission to write to certain directories
-There are times when Apache will need to be able to write to two of the directories - specifically the /drawings (maps of your data centers) and /pictures (pictures of your devices) folders. To do this, we need to set the group for those folders to the group that Apache is running as. On a default Ubuntu 15.04 server, that group is called www-data.
+# Download and unpack openDCIM
+cd /var/www
+sudo wget http://opendcim.org/packages/openDCIM-4.0.1.tar.gz
+sudo tar zxpvf openDCIM-4.0.1.tar.gz
+sudo ln -s openDCIM-4.0.1 dcim
 
-$ sudo chgrp -R www-data /var/www/dcim/pictures /var/www/dcim/drawings
-Create your openDCIM DB and point openDCIM at it
-You will need to either have your dba perform this step for you or you will need to know the MySQL root password (you are prompted to set it during installation). For our example, it is rootpw.
+# Set permissions for Apache
+sudo chgrp -R www-data /var/www/dcim/pictures /var/www/dcim/drawings
 
-$ mysql -u root -prootpw
-mysql> create database dcim;
-mysql> grant all on dcim.* to 'dcim'@'localhost' identified by 'dcim';
-mysql> quit
-This will create a database called 'dcim' and also grant full access on it to a user who can only access the db locally and using the password of 'dcim'.
+# Create openDCIM database and user
+sudo mysql -u root -p"$DB_ROOT_PASSWORD" <<EOF
+CREATE DATABASE $DB_NAME;
+GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$DB_PASSWORD';
+quit
+EOF
 
-$ cd /var/www/dcim
-$ cp db.inc.php-dist db.inc.php
-If you are using a locally hosted MySQL database called 'dcim' with a user and password combination of 'dcim'/'dcim' then there is no need to edit the file once you have copied it over. If you have different values, please update the file accordingly.
+# Copy and configure db.inc.php
+cd /var/www/dcim
+sudo cp db.inc.php-dist db.inc.php
+sudo sed -i "s/\$dbhost = 'localhost';/\$dbhost = 'localhost';/" db.inc.php
+sudo sed -i "s/\$dbname = 'dcim';/\$dbname = '$DB_NAME';/" db.inc.php
+sudo sed -i "s/\$dbuser = 'dcim';/\$dbuser = '$DB_USER';/" db.inc.php
+sudo sed -i "s/\$dbpass = 'dcimpassword';/\$dbpass = '$DB_PASSWORD';/" db.inc.php
 
-Configure Apache
-By default, Apache only runs on port 80 (http) and serves the content in directory /var/www/html. We want SSL encryption and we don't want the default site, either.
+# Configure Apache
+sudo sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/var\/www\/dcim/' /etc/apache2/sites-available/default-ssl.conf
+sudo sed -i '/DocumentRoot \/var\/www\/dcim/a \
+    <Directory "/var/www/dcim"> \
+        Options All \
+        AllowOverride All \
+        Require all granted \
+    </Directory>' /etc/apache2/sites-available/default-ssl.conf
+sudo sed -i 's/ErrorLog ${APACHE_LOG_DIR}\/error.log/ErrorLog ${APACHE_LOG_DIR}\/dcim-error.log/' /etc/apache2/sites-available/default-ssl.conf
+sudo sed -i 's/CustomLog ${APACHE_LOG_DIR}\/access.log combined/CustomLog ${APACHE_LOG_DIR}\/dcim-access.log/' /etc/apache2/sites-available/default-ssl.conf
 
-$ cd /etc/apache2/sites-available
-$ sudo nano default-ssl.conf
-Change the line that starts with DocumentRoot, plus add the following lines:
+# Set up user access
+sudo htpasswd -cb /var/www/opendcim.password dcim dcim
+sudo cp /var/www/opendcim.password /var/www/dcim/.htpasswd
 
-DocumentRoot /var/www/dcim
+# Enable SSL and other required modules
+sudo a2enmod ssl
+sudo a2enmod rewrite
+sudo a2ensite default-ssl
 
-<Directory "/var/www/dcim">
-    Options All
-    AllowOverride All
-    Require all granted
-</Directory>
-It is also suggested that you change the following two lines further down in the configuration file:
+# Restart Apache
+sudo service apache2 restart
 
-ErrorLog ${APACHE_LOG_DIR}/dcim-error.log
-CustomLog ${APACHE_LOG_DIR}/dcim-access.log
-Save the file and we're almost there.
-
-Set up user access for your website
-There are a lot of ways to control access to your web server and we are only going to cover the most basic. You can search the web for ways to integrate Apache with LDAP or Active Directory, but at that point it is an Apache issue, not an openDCIM issue.
-
-To set up the most basic authentication possible:
-
-$ nano /var/www/dcim/.htaccess
-
-AuthType Basic
-AuthName "openDCIM"
-AuthUserFile /var/www/opendcim.password
-Require valid-user
-Save that file and Apache will know how to ask users to log in. We have one last thing to do before we can pull up the website.
-
-Finalize the Apache configuration and restart it
-Issue the following commands to enable the site and modules that we need. Most of these commands will tell you to restart apache after you issue them - ignore that, as you are restarting apache as the very last step.
-
-$ sudo htpasswd -cb /var/www/opendcim.password dcim dcim
-$ sudo a2enmod ssl
-$ sudo a2enmod rewrite
-$ sudo a2ensite default-ssl
-$ sudo service apache2 restart
+echo "openDCIM installation complete."
